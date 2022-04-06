33eb49506d7a3b037bb3ac1d265ef7e6548d8891
Isis <blancoisis1@gmail.com>
2022-04-06 13:43:48 -0400

    commit tree 927a216421db80543122dd2339e67cf91356c377

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
+let num1=31;
+let num2=2;
+let multiply=62;
 
-// Write your code here
+function multiply(num1, num2) {
+  if (num1*num2===62) {
+      return ("You are correct!")
+  }
+}
+
+console.log(multiply)
+
+let random=1
+
+function random(isInteger){
+    if (isInteger => 0)
+    return (Number)
+}
+
+
+let mod=4
+let num3=5
+let num4=24
+
+function mod(num3, num4){
+    if(num3/num4===4)
+    return (mod)
+}
+
+let max=20
+
